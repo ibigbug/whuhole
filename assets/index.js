7379995795695d015fed60a7b55af7123354cb5b
ibigbug <xiaobayuwei@gmail.com>
2014-02-07 15:31:07 +0800

    Add moment.js & remove static folder

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,25 @@
 var $ = require('bootstrap');
+var moment = require('moment');
+
+
+$('.timeago').each(function (index, element) {
+  $(element).html(moment($(this).attr('datetime')).fromNow());
+});
+
 $('.js-logout').click(function () {
   var url = $(this).data('url');
 
-  var logout_xhr = $.ajax({
+  $.ajax({
     type: 'POST',
     url: url
   }).done(function () {
     location.reload();
-  });;
+  });
 });
 
 $('.js-like').click(function () {
   var url = $(this).data('url');
-  var like_xhr = $.ajax({
+  $.ajax({
     type: 'POST',
     url: url
   }).done(function () {
